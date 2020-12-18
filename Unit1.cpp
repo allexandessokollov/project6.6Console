a5826731702e14de6997b77cc3990b128feb327e
allexandessokollov <allexandersokollov@gmail.com>
2020-12-18 20:38:43 +0300

    fdshofiudws

--- Unit1.cpp
+++ Unit1.cpp
@@ -10,40 +10,37 @@
 #include <conio.h>
 #include <stdlib.h>
 
+const int MAX_MATRIX_SIZE = 7;
+int const MAX_RANDOM_VALUE = 25;
+int const MIN_RANDOM_VALUE = -25;
 
 void fillArrKeyboard(int *arr[], int nSize, int mSize);
 void fillArrRandomly(int *arr[], int nSize, int mSize);
-bool alreadyExist(int arr[], int arrSize, int value);
+bool alreadyExist(int arr[], int countOfElemToCheck, int value);
 
-const int MAX_MATRIX_SIZE = 7;
 int main()
 {
 
-        int nValue, mValue, userChoice, tmpArrSize, counter = 0;
+        int nValue, mValue, userChoice;
 
         printf("Enter matrix size N x M\n");
         scanf("%d %d", &nValue, &mValue);
 
-        while (nValue < 0 || mValue < 0)
+        while (nValue < 0 ||
+         mValue < 0 ||
+         nValue > MAX_MATRIX_SIZE ||
+         mValue > MAX_MATRIX_SIZE)
         {
-            printf("N and M should be more than 0. Enter again");
-            scanf("%d %d", nValue, mValue);
+            printf("N and M should be more than 0 and less than 7! Enter again\n");
+            scanf("%d %d", &nValue, &mValue);
         }
-        while(nValue > MAX_MATRIX_SIZE || mValue > MAX_MATRIX_SIZE)
-        {
-            printf("\nmaximum size is 7 x 7\n");
-
-            nValue =  MAX_MATRIX_SIZE;
-            mValue =  MAX_MATRIX_SIZE;
-
-        }
-
 
         int **arr = new int*[nValue];
         for(int i = 0; i < nValue; i++)
         {
             arr[i] = new int[mValue];
         }
+
 
         printf("Enter 1 to fill array using keyboard and 2 for random\n");
         scanf("%d", &userChoice);
@@ -71,7 +68,7 @@ int main()
 
         printf("\n\n");
 
-        tmpArrSize =  (nValue) * (mValue);
+        int tmpArrSize =  (nValue) * (mValue);
         int *tmpArr = new int[tmpArrSize];
 
         int toInitialize = -99999;
@@ -80,7 +77,7 @@ int main()
             tmpArr[j] = toInitialize;
         }
 
-
+        int counter = 0;
 
         for(int i = 0; i < nValue; i++)
         {
@@ -88,7 +85,7 @@ int main()
             {
                 int temp = arr[i][j];
 
-                if(alreadyExist(tmpArr, tmpArrSize, temp))
+                if(alreadyExist(tmpArr, counter, temp))
                 {
                     continue;
                 }
@@ -104,11 +101,16 @@ int main()
         printf("\n\nanswer is: %d \n\n", counter);
     
 
-
         printf("\n\npress any int number to close");
         int close;
         scanf("%d", &close);
 
+        for(int i = 0; i < mValue; i++)
+        {
+            delete []arr[i];
+        }
+        delete []arr;
+        delete []tmpArr;
 
         return 0;
 }
@@ -130,28 +132,27 @@ void fillArrKeyboard(int *arr[], int nSize, int mSize)
 void fillArrRandomly(int *arr[], int nSize, int mSize)
 {
 
-    const int forRandomCorrection = -25,
-    forRandom = 50;
+
     for(int i = 0; i < nSize; i++)
     {
         for(int j = 0; j < mSize; j++)
         {
-            arr[i][j] = random(forRandom) + forRandomCorrection;
+            arr[i][j] = random(MAX_RANDOM_VALUE - MIN_RANDOM_VALUE) + MIN_RANDOM_VALUE;
         }
     }
 }
 
 
-bool alreadyExist(int arr[], int arrSize, int value)
+bool alreadyExist(int arr[], int countOfElemToCheck, int value)
 {
-       for(int i = 0; i < arrSize; i++)
+       for(int i = 0; i < countOfElemToCheck; i++)
        {
             if(arr[i] == value)
             {
                 return true;
             }
        }
-       
+
        return false;
 }
 
